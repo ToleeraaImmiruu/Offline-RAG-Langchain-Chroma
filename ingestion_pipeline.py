a335de17fa6b38ef7826af28962d932f60c047d9
Tolera Imiru <toleeraa2024@gmail.com>
2026-03-21 12:27:32 +0300

    Update app.py

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -19,7 +19,6 @@ def load_documents(docs_path="docs"):
         loader_cls=TextLoader,
         loader_kwargs={"encoding": "utf-8"}
     )
-
     documents = loader.load()
     if not documents:
         raise FileNotFoundError("No .txt files found")

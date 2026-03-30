f02f06709fc02233b41dbd1d087e8d9004fa7934
Tolera Imiru <toleeraa2024@gmail.com>
2026-03-30 09:27:21 +0300

    Update app.py

--- retrival_pipeline.py
+++ retrival_pipeline.py
@@ -10,7 +10,6 @@ load_dotenv()
 # 1️⃣ Load vector store (OFFLINE)
 # ===============================
 persistent_directory = "db/chroma_db"
-
 # Use offline embeddings
 embedding_model = HuggingFaceEmbeddings(
     model_name="all-MiniLM-L6-v2"  # small, fast embeddings
